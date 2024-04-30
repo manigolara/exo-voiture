8644648f0619095a248cf4aeb94656db9813c50a
Manigo <maniwai@outlook>
2024-04-30 15:12:38 +0200

    updated

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 var voiture = {
-    type: "citadine",
-    couleur: "red",
-    
-}
+  type: "citadine",
+  couleur: "red",
+};
+window.onload = function () {
+  document
+    .getElementById("formulaire")
+    .addEventListener("submit", function (event) {
+      //var audio = new Audio("https://lasonotheque.org/UPLOAD/mp3/0600.mp3");
+      //audio.play();
+      event.preventDefault();
+      afficherVoiture();
+    });
+  function afficherVoiture() {
+    var selectedCar = document.getElementById("car-select").value;
+    var selectedColor = document.getElementById("color-select").value;
+
+    console.log(selectedCar, selectedColor);
+    if (selectedCar && selectedColor) {
+      var voitureDiv = document.getElementById("voiture");
+      var newImg = document.createElement("img");
+      newImg.src = "images/" + selectedCar + ".png";
+      newImg.style.backgroundColor = selectedColor;
+      voitureDiv.appendChild(newImg);
+    }
+  }
+};
